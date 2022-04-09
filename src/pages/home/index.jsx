57d13cb52661c2aa2e6c6d01e0fed6cd522c9b87
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-04-09 00:47:25 -0300

    Mobile Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import imageSrc from "./assets/hoodie.png";
+import { Link } from "react-router-dom";
+
+const Main = styled.main`
+  padding: 0 25px 85px;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+`;
+
+const BackgroundCircle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  filter: blur(40px);
+  z-index: -1;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: ${(props) => (props.yPos ? `calc(50% - ${props.yPos})` : "50%")};
+  left: ${(props) => (props.xPos ? `calc(50% + ${props.xPos})` : "50%")};
+  background-color: ${(props) =>
+    props.backgroundColor ? props.backgroundColor : "#B4ADEA"};
+  opacity: ${(props) => (props.opacity ? props.opacity : 0.5)};
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 500px;
+  object-fit: cover;
+  object-position: calc(50% - 60px) 50%;
+  position: relative;
+  bottom: 50px;
+`;
+
+const Text = styled.p`
+  font-size: 0.875rem;
+  font-weight: 700;
+  width: 75%;
+  position: relative;
+  bottom: 50px;
+`;
+
+const Button = styled(Link)`
+  width: 139px;
+  height: 44px;
+  background: #9381ff;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
 
 export default function Home() {
+  const isTablet = useMediaQuery("(max-width:768px)");
+
   return (
-    <div>Home</div>
-  )
+    <Main>
+      <Title>
+        Lorem ipsum <br /> dolor sit.
+      </Title>
+      {isTablet ? (
+        <>
+          <ImageContainer>
+            <Image src={imageSrc} />
+            <BackgroundCircle xPos="-100px" yPos="60px" />
+            <BackgroundCircle xPos="80px" yPos="-50px" opacity="0.8" />
+            <BackgroundCircle
+              backgroundColor="#ECD444"
+              xPos="50px"
+              yPos="80px"
+            />
+          </ImageContainer>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            consequat massa vel vestibulum auctor. Nullam rhoncus malesuada
+            orci, ac mollis.
+          </Text>
+        </>
+      ) : (
+        <></>
+      )}
+      <Button to="shop">Shop now</Button>
+    </Main>
+  );
 }
